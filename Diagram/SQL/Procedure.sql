4747e5f2da9f2fad044314bc37297a1129db1b9b
MauroGuida <57829432+MauroGuida@users.noreply.github.com>
2020-03-12 09:46:34 +0100

    +Aggiunta tabella procedure SQL

---
+++ Diagram/SQL/Procedure.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE resetPassword(INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  Update utente
+  set password=(select substr(md5(random()::text), 0, 10))
+  where codutente=$1;
+
+  COMMIT;
+END;
+$$;
