e55e782b403d444fc4d6c0ea992c06734cf89068
MauroGuida <57829432+MauroGuida@users.noreply.github.com>
2020-03-15 09:37:56 +0100

    +Aggiunta gestione stelle per il Locale

--- PlaceHub_Java/src/oggetti/Locale.java
+++ PlaceHub_Java/src/oggetti/Locale.java
@@ -7,36 +7,31 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.imageio.ImageIO;
+import javax.swing.border.LineBorder;
+
+import res.WrapLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.border.LineBorder;
 
 public class Locale extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private BufferedImage immagine;
-	private JLabel lblStelle;
 	
 	public Locale(String nomeBusiness, String indirizzo, int numStelle, String urlImmagine) {
 		setBackground(Color.WHITE);
 		setSize(400,250);
 		setVisible(true);
 		setBorder(new LineBorder(Color.DARK_GRAY,1));
+		
 		JLabel labelNome = new JLabel(nomeBusiness);
 		labelNome.setFont(new Font("Roboto", Font.PLAIN, 17));
 		
 		JLabel labelLuogo = new JLabel(indirizzo);
 		labelLuogo.setFont(new Font("Roboto", Font.PLAIN, 17));
-		
-		lblStelle = new JLabel();
-		lblStelle.setIcon(new ImageIcon(Locale.class.getResource("/Icone/stella.png")));
-		lblStelle.setFont(new Font("Roboto", Font.PLAIN, 13));
 		
 		JLabel labelmmagine = new JLabel();
 		try {
@@ -47,7 +42,17 @@ public class Locale extends JPanel {
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
-		labelmmagine.setBackground(Color.RED);
+		
+		JPanel stelle = new JPanel();
+		stelle.setBackground(Color.WHITE);
+		stelle.setLayout(new WrapLayout(WrapLayout.LEFT, 1 ,1));
+
+		for(int i=0; i<numStelle; i++) {
+			JLabel stellaPiena = new JLabel();
+			stellaPiena.setIcon(new ImageIcon(Locale.class.getResource("/Icone/stella.png")));
+			stelle.add(stellaPiena);
+		}
+		
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -55,11 +60,13 @@ public class Locale extends JPanel {
 					.addGap(12)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(labelNome, GroupLayout.PREFERRED_SIZE, 185, GroupLayout.PREFERRED_SIZE)
-							.addGap(10)
-							.addComponent(labelLuogo, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE))
+							.addComponent(labelNome, GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE)
+							.addGap(189))
 						.addComponent(labelmmagine, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
-						.addComponent(lblStelle, GroupLayout.PREFERRED_SIZE, 185, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(195)
+							.addComponent(labelLuogo, GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE))
+						.addComponent(stelle, GroupLayout.PREFERRED_SIZE, 170, GroupLayout.PREFERRED_SIZE))
 					.addGap(12))
 		);
 		groupLayout.setVerticalGroup(
@@ -71,12 +78,14 @@ public class Locale extends JPanel {
 							.addGap(179)
 							.addComponent(labelNome, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
 						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(labelmmagine, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+							.addGap(23))
+						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(179)
-							.addComponent(labelLuogo, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
-						.addComponent(labelmmagine, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE))
-					.addComponent(lblStelle, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
+							.addComponent(labelLuogo, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)))
+					.addGap(6)
+					.addComponent(stelle, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
 		);
 		setLayout(groupLayout);
-		
 	}
 }
